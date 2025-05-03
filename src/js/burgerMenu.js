39ebed0e56cd7ce9e1999b830125692e661bef9a
Di <diana8burkut@gmail.com>
2025-05-03 20:17:07 +0200

    added button svg`s change logic

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,9 +1,11 @@
 const burgerBtnEl = document.querySelector('[data-button]');
 const burgerMenuEl = document.querySelector('[data-visible]');
-
+const burgerUseEl = document.querySelector('[data-el="burger-use"]');
 burgerBtnEl.addEventListener('click', e => {
   const isOpen = burgerMenuEl.dataset.visible === 'open';
 
   burgerMenuEl.dataset.visible = isOpen ? 'close' : 'open';
   burgerBtnEl.dataset.button = isOpen ? 'burger-open' : 'burger-close';
+  const iconId = isOpen ? '#burger-menu' : '#icon-icon-';
+  burgerUseEl.setAttribute('href', `/img/icons.svg${iconId}`);
 });
